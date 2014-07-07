8390dc44cdd9baa1267c76ca90bf6130e00f81c3
Yuin Ee <8dgree@gmail.com>
2014-07-06 23:26:51 -0700

    Adding working in progress canvas branch
    - Added markup
    - Added JS functions for canvas

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
+(function() {
 
+  var canvas = document.getElementById('koi');
+
+  // resize the canvas to fill browser window dynamically
+  window.addEventListener('resize', resizeCanvas, false);
+
+  function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    /**
+     * Drawings need to be inside this function otherwise they will be reset when 
+     * the browser window is resized and the canvas will be cleared.
+     */
+    drawKoi();
+  }
+  resizeCanvas();
+
+  function drawKoi() {
+  }
+
+})();
